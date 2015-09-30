fef2c8c600676a58af600070842f8eb1ac1b437e
Mars Saxman <mars@redecho.org>
2015-09-29 17:50:08 -0700

    prettied up the build system a bit; no need for an extra subdir under obj

--- build/compile.sh
+++ build/compile.sh
@@ -29,24 +29,25 @@ mkdir -p $OBJDIR
 for dep in $(find $OBJDIR -type f -name "*.d"); do
 	depfiledata=$(cat $dep)
 	objtarget=${depfiledata%%:*}
-	if [[ -e $objtarget ]]
-	then
-		deplist=${depfiledata##*:}
-		for checkfile in $deplist; do
-			# Ignore the backslash line-continuation entries gcc puts in the
-			# dep file
-			if [ "$checkfile" = "\\" ] ; then
-				continue
-			fi
-			# If the obj file depends on a file which is newer, delete it.
-			# If the obj file depends on a file which does not exist, that is
-			# also a good reason to delete it.
-			if [ $checkfile -nt $objtarget -o ! -e $checkfile ] ; then
-				rm $objtarget
-				break
-			fi
-		done
+	if [[ ! -e $objtarget ]] ; then
+		# If the .o file doesn't exist, we already know it needs compilation.
+		continue
 	fi
+	deplist=${depfiledata##*:}
+	for checkfile in $deplist; do
+		# Ignore the backslash line-continuation entries gcc puts in the
+		# dep file
+		if [ "$checkfile" = "\\" ] ; then
+			continue
+		fi
+		# If the obj file depends on a file which is newer, delete it.
+		# If the obj file depends on a file which does not exist, that is
+		# also a good reason to delete it.
+		if [ $checkfile -nt $objtarget -o ! -e $checkfile ] ; then
+			rm $objtarget
+			break
+		fi
+	done
 done
 
 # Examine each source file. If there is not already a .o file for it in the
@@ -56,39 +57,37 @@ done
 errorfound=0
 srcfiles=$(find $SRCDIR -type f -name "*.cpp" -o -name "*.c" -o -name "*.s")
 for src in $srcfiles; do
-	# make up an obj file path in our obj subdirectory
-	# Strip off any leading "./" which might happen to be present
-	src="${src#\./}"
+	# Get the source file path relative to the source directory.
+	srcrel="${src#$SRCDIR/}"
+	# Get the name extension, indicating source language.
 	extension="${src##*.}"
-	relpath="${src%/*.*}"
-	mkdir -p "$OBJDIR/$relpath"
-	objname="${src%.*}.o"
-	obj="$OBJDIR/$objname"
-	depname="${src%.*}.d"
-	dep="$OBJDIR/$depname"
+	# Truncate the extension and generate equivalent .o and .d paths, under
+	# the obj directory instead.
+	objrel="$OBJDIR/${srcrel%.*}"
+	obj="$objrel.o"
+	dep="$objrel.d"
+	# If this src file lives under a subdirectory of the srcdir, create an
+	# equivalent subdirectory path under the objdir.
+	subdir="${objrel%/*}"
+	if [ -n $subdir ]; then
+		mkdir -p $subdir
+	fi
 
 	# Do we need to recompile the source file?
 	if [[ -e $obj && -e $dep ]] ; then
-		# an object file exists, so no need to recompile
-		# echo "using $objname"
-		true
-	else
-		# no object file, so we'll compile one
-		echo "compiling $src..."
+		continue
+	fi
+	echo "compiling $srcrel..."
 
-		# only specify C99 if we are not compiling a c++ file
-		if [[ $extension == "c" ]]; then
-			stdarg="-std=$STDC"
-		elif [[ $extension == "cpp" ]]; then
-			stdarg="-std=$STDCPP"
-		fi
+	# only specify C99 if we are not compiling a c++ file
+	if [[ $extension == "c" ]]; then
+		stdarg="-std=$STDC"
+	elif [[ $extension == "cpp" ]]; then
+		stdarg="-std=$STDCPP"
+	fi
 
-		if $CPP $CCFLAGS -MD $stdarg -I$SRCDIR -c $src -o $obj
-		then
-			echo -n ""
-		else
-			errorfound=1
-		fi
+	if $CPP $CCFLAGS -MD $stdarg -I$SRCDIR -c $src -o $obj; then
+		errorfound=1
 	fi
 done
 
