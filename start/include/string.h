54e979660116a8c34d65975f0c1764c1c9de051e
Mars Saxman <mars@redecho.org>
2015-10-03 22:08:09 -0700

    cleaned up & factored out the C runtime support library, now called 'libstart'

---
+++ start/include/string.h
@@ -0,0 +1,20 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int memcmp(const void*, const void*, size_t);
+void* memcpy(void* __restrict, const void* __restrict, size_t);
+void* memmove(void*, const void*, size_t);
+void* memset(void*, int, size_t);
+size_t strlen(const char*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
