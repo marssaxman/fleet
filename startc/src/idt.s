e705d65a862fa75cfd2265dc0778f82b2f2de1c4
Mars Saxman <mars@redecho.org>
2015-10-04 18:11:56 -0700

    why not initialize the IDT with the values we're going to give it?

--- startc/src/idt.s
+++ startc/src/idt.s
@@ -1,16 +1,35 @@
-# Set up a full-size, empty IDT which can be configured with interrupt vectors.
+# Set up an IDT and configure its selectors.
+# We will configure the interrupts corresponding to CPU exceptions and to IRQs,
+# after reconfiguring the PICs to move them up to 0x20. Other interrupts will
+# be left blank. We will use segment 0x08 as defined in the GDT and gates will
+# be configured as interrupts, not traps.
+
+.macro idt_entry selector, flags
+	.hword 0x0000	# offset_low
+	.hword \selector
+	.byte 0x00		# zero
+	.byte \flags
+	.hword 0x0000	# offset_high
+.endm
+.macro active_entries count
+	.rept \count
+	idt_entry 0x0008, flags=0x8E
+	.endr
+.endm
+.macro blank_entries count
+	.rept \count
+	idt_entry 0x0000, flags=0x00
+	.endr
+.endm
 
 .section .data
 .align 8
 .global _idt
 _idt:
-	.rept 256
-	.hword 0x0000	# offset_low
-	.hword 0x0000	# selector
-	.byte 0x00		# zero
-	.byte 0x00		# flags
-	.hword 0x0000	# offset_high
-	.endr
+	active_entries 0x14	# 0x00..0x13: CPU exceptions
+	blank_entries 0x0C  # 0x14..0x1F: reserved but unused
+	active_entries 0x10 # 0x20..0x2F: remapped IRQs
+	blank_entries 0xD0  # 0x30..0xFF: unused
 .global _idtr
 _idtr:
 	.hword 0x07FF	# size of table - 1
