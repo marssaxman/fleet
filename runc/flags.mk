ef355761a5a8be54cf219692b4da4b19509f3d52
Mars Saxman <mars@redecho.org>
2015-10-19 16:39:57 -0700

    executables can now rebuild the CRT libs they depend on

--- runc/flags.mk
+++ runc/flags.mk
@@ -1,5 +1,7 @@
 RUNC_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 include $(RUNC_DIR)/../startc/flags.mk
+CRT_LIBS += $(RUNC_DIR)/librunc.a
+
 CCFLAGS += -isystem $(RUNC_DIR)/include
 LDFLAGS += --library-path=$(RUNC_DIR)
 LDFLAGS += --library=runc
