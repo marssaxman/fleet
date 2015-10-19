e5e9cf2ddbadf5affc198c4de110b384e1dd9d30
Mars Saxman <mars@redecho.org>
2015-10-19 15:34:31 -0700

    reorganized build system, all moved to a common makefile

--- runc/flags.mk
+++ runc/flags.mk
@@ -1,9 +1,6 @@
 RUNC_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
-
 include $(RUNC_DIR)/../startc/flags.mk
-
 CCFLAGS += -isystem $(RUNC_DIR)/include
-
 LDFLAGS += --library-path=$(RUNC_DIR)
 LDFLAGS += --library=runc
 
