029c795086b98458e3452066f1c5c4316f62e8c0
Mars Saxman <mars@redecho.org>
2015-10-19 13:11:16 -0700

    moved definitions of all the common build flags into common .mk files

---
+++ core/flags.mk
@@ -0,0 +1,10 @@
+CORE_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+
+include $(CORE_DIR)/../runc/flags.mk
+
+CCFLAGS += -isystem $(CORE_DIR)/include
+
+LDFLAGS += --library-path=$(CORE_DIR)
+LDFLAGS += --library=core
+
+
