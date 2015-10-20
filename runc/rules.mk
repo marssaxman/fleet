6b64c5702a084be66854b52f24d7a6647d45597d
Mars Saxman <mars@redecho.org>
2015-10-20 09:05:05 -0700

    squeaking just a little closer to making this work

--- runc/rules.mk
+++ runc/rules.mk
@@ -1,10 +1,17 @@
-# Standard build script for runc submodules.
-# The last word on the makefile list is this rules.mk, so we can find the
-# runc library's root directory.
-RUNC := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
+# Standard build script for runc submodules, to be invoked by the stub
+# makefile in each module directory.
 
-# We expect that the module which included this makefile has pulled the same
-# trick and gotten its own $(MODULE) path.
+# This file is currently the last one on the makefile list, so we can use it
+# to get the base path for the runc library.
+RUNC := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+
+# The previous item on the list is the makefile that included us, which lives
+# in the module's directory, so we can use that to find out what the module's
+# name is and where its files live.
+MAKEFILE_COUNT = $(words $(MAKEFILE_LIST))
+DUMMY_LIST = $(addprefix "foo ", $(MAKEFILE_LIST))
+PREV_MAKEFILE = $(word $(MAKEFILE_COUNT), $(DUMMY_LIST))
+MODULE := $(realpath $(dir $(PREV_MAKEFILE)))
 
 # Load the standard target settings
 include $(RUNC)/../target.mk
@@ -13,17 +20,18 @@ include $(RUNC)/../target.mk
 CFLAGS += -isystem $(RUNC)/include -isystem $(FLEETBASE)/startc/include
 
 MODNAME := $(notdir $(MODULE:%/=%))
-LIBNAME := $(addprefix lib, $(addsuffix .a, $(MODNAME)))
-OUTLIB := $(MODULE)$(LIBNAME)
-SRCS := $(wildcard $(MODULE)src/*.c)
-OBJS := $(subst /src/,/obj/,$(basename $(SRCS))).o
+OUTLIB := $(MODULE)/$(addprefix lib, $(MODNAME:%=%.a))
+SRCS := $(wildcard $(MODULE)/src/*.c)
+OBJS := $(subst /src/,/obj/,$(SRCS:%.c=%.o))
 
 # We will use auto dependency tracking.
 DEPS := $(OBJS:%.o=%.d)
 CFLAGS += -MD -MP
 -include $(DEPS)
 
-all: $(OUTLIB)
+all: lib $(OBJS)
+
+lib: $(OUTLIB)
 
 $(OUTLIB): $(OBJS)
 	ar rcs $@ $^
