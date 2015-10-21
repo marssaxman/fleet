df8bc142ecd56a0f1a256e7145a7925425b6ac5a
Mars Saxman <mars@redecho.org>
2015-10-20 18:55:28 -0700

    we don't actually need a separate check module now

--- runc/test/src/check.h
+++
@@ -1,6 +0,0 @@
-#ifndef CHECK_H
-#define CHECK_H
-
-void suite_exit();
-
-#endif //CHECK_H
--- runc/test/src/main.c
+++ runc/test/src/main.c
@@ -1,9 +1,124 @@
 #include "debug-write.h"
-#include "check.h"
+
+static const char *current_func = 0;
+static unsigned current_fails = 0;
+
+static void print_dec(int num)
+{
+	char buf[12];
+	int i = 12;
+	buf[--i] = '\0';
+	while (num) {
+		buf[--i] = (num % 10) + '0';
+		num /= 10;
+	}
+	debug_write(&buf[i]);
+}
+
+static void write_test_conclusion()
+{
+	if (0 == current_func) return;
+	// We have finished the previous function and have just passed the first
+	// test in a new function. If all tests in the previous function passed,
+	// print a message to that effect.
+	debug_write(current_func);
+	debug_write(": ");
+	if (0 == current_fails) {
+		debug_write("pass");
+	} else {
+		print_dec(current_fails);
+		debug_write(" check");
+		if (current_fails > 1) {
+			debug_putc('s');
+		}
+		debug_write(" failed");
+	}
+	debug_putc('\n');
+}
+
+static void check_test_conclusion(const char *func)
+{
+	// did the check we just made come from the same function as the previous
+	// one? if so, the test has not ended. if we are in a new function, then
+	// the previous one ended and we should print its status info.
+	if (current_func == func) return;
+	write_test_conclusion();
+	current_fails = 0;
+	current_func = func;
+}
+
+static void pass(const char *func)
+{
+	check_test_conclusion(func);
+}
+
+static void fail(const char *func, int line, const char *cond)
+{
+	check_test_conclusion(func);
+	current_fails++;
+	debug_write("FAIL ");
+	debug_write(func);
+	debug_write("@");
+	print_dec(line);
+	debug_write(": ");
+	debug_write(cond);
+	debug_putc('\n');
+}
+
+void check(int flag, const char *cond, const char *func, int line)
+{
+	if (flag) pass(func);
+	else fail(func, line, cond);
+}
+
+void check_mem(
+		const void *actual,
+		const void *expect,
+		int bytes,
+		const char *func,
+		int line)
+{
+	for (int i = 0; i < bytes; ++i) {
+		if (((unsigned char*)actual)[i] != ((unsigned char*)expect)[i]) {
+			fail(func, line, "");
+			debug_write("\texpect: ");
+			debug_print_buffer(expect, bytes);
+			debug_write("\n\tactual: ");
+			debug_print_buffer(actual, bytes);
+			debug_putc('\n');
+			return;
+		}
+	}
+	pass(func);
+}
+
+void check_str(
+		const char *actual,
+		const char *expect,
+		int max,
+		const char *func,
+		int line)
+{
+	const char *aptr = actual;
+	const char *eptr = expect;
+	for (int i = 0; i < max; ++i) {
+		if (*aptr != *eptr++) {
+			fail(func, line, "");
+			debug_write("\texpect: ");
+			debug_write(expect);
+			debug_write("\n\tactual: ");
+			debug_write(actual);
+			debug_putc('\n');
+			return;
+		}
+		if (!*aptr++) break;
+	}
+	pass(func);
+}
 
 int main()
 {
-	suite_exit();
+	write_test_conclusion();
 	debug_write("END runc test suite\n");
 	return 0;
 }
