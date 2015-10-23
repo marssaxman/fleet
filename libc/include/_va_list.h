3dbed02181b521adb87f970f5ec0f7c62ae38a84
Mars Saxman <mars@redecho.org>
2015-10-22 19:27:48 -0700

    split va_list into a separate header so we can include it from stdio

---
+++ libc/include/_va_list.h
@@ -0,0 +1,6 @@
+#ifndef __VA_LIST_H
+#define __VA_LIST_H
+
+typedef __builtin_va_list va_list;
+
+#endif __VA_LIST_H
--- libc/include/stdarg.h
+++ libc/include/stdarg.h
@@ -7,11 +7,12 @@
 #ifndef _STDARG_H
 #define _STDARG_H
 
+#include <_va_list.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef __builtin_va_list va_list;
 #define va_start(v,l) __builtin_va_start(v,l)
 #define va_end(v) __builtin_va_end(v)
 #define va_arg(v,l) __builtin_va_arg(v,l)
