4ef168e0b6988c7da49d75a87c65f355267e5c2b
Mars Saxman <mars@redecho.org>
2015-10-23 09:07:30 -0700

    one step at a time, now have _libc_main()

--- libc/system/libc.h
+++ libc/system/libc.h
@@ -1,17 +1,20 @@
 #ifndef _LIBC_SYSTEM_H
 #define _LIBC_SYSTEM_H
 
+// Startup initialization interface
+
 #include "stream.h"
 
-struct _libc_config
-{
-	struct _libc_stream *stdin;
-	struct _libc_stream *stdout;
-	struct _libc_stream *stderr;
-	const char *args;
-};
+// The runtime host must populate these stream objects before invoking main.
+extern _libc_stream _stdin_stream;
+extern _libc_stream _stdout_stream;
+extern _libc_stream _stderr_stream;
 
-void _libc_init(const struct _libc_config*);
+// The runtime should call _libc_main() when the environment is ready. It
+// will set up the argument vector and invoke main().
+void _libc_main(const char *cmdline);
+
+extern int main(int argc, char **argv);
 
 #endif //_LIBC_SYSTEM_H
 
