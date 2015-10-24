6a8e18c8cdd63fb815dc9a8a798eed3eb562ae2b
Mars Saxman <mars@redecho.org>
2015-10-23 23:38:39 -0700

    we don't actually need those link.mk files

--- kernel/link.mk
+++
@@ -1,6 +0,0 @@
-KERNEL_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
-include $(KERNEL_DIR)/target.mk
-CFLAGS += -isystem $(KERNEL_DIR)/include
-LDLIBS += -lkernel
-LDFLAGS += -L$(KERNEL_DIR)
-
--- libc/link.mk
+++
@@ -1,7 +0,0 @@
-# Link your project against libc by including this file in your Makefile.
-LIBC_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
-include $(LIBC_DIR)/target.mk
-CFLAGS += -isystem $(LIBC_DIR)/include
-LDLIBS += -lc
-LDFLAGS += -L$(LIBC_DIR)
-
