0b60f85936e764f7701232db66ce0d5b8c7f981c
Mars Saxman <mars@redecho.org>
2015-12-01 16:59:21 -0800

    added copyright & license headers

--- libc/include/math.h
+++ libc/include/math.h
@@ -1,3 +1,9 @@
+// Copyright (C) 2015 Mars Saxman. All rights reserved.
+// Permission is granted to use at your own risk and distribute this software
+// in source and binary forms provided all source code distributions retain
+// this paragraph and the above copyright notice. THIS SOFTWARE IS PROVIDED "AS
+// IS" WITH NO EXPRESS OR IMPLIED WARRANTY.
+
 #ifndef _MATH_H
 #define _MATH_H
 
--- libc/src/string/memmove.c
+++ libc/src/string/memmove.c
@@ -1,3 +1,9 @@
+// Copyright (C) 2015 Mars Saxman. All rights reserved.
+// Permission is granted to use at your own risk and distribute this software
+// in source and binary forms provided all source code distributions retain
+// this paragraph and the above copyright notice. THIS SOFTWARE IS PROVIDED "AS
+// IS" WITH NO EXPRESS OR IMPLIED WARRANTY.
+
 #include <string.h>
 
 void *memmove(void *dest, const void *src, size_t n)
