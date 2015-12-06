39d86e72412a69409a2197068d405b15db4c3e7a
Mars Saxman <mars@redecho.org>
2015-12-05 16:17:05 -0800

    basic integer string formatting

--- libc/src/stdio/format.c
+++ libc/src/stdio/format.c
@@ -19,6 +19,47 @@ bool _format_done(struct format_state *state)
 	return '\0' == *state->fmt;
 }
 
+static unsigned utoa(char *buf, uint64_t i, int radix, const char *digits)
+{
+	// Write the string in least-to-most significant order, then reverse it.
+	unsigned len = 0;
+	do {
+		*buf++ = digits[i % radix];
+		i /= radix;
+		len++;
+	} while (i > 0);
+	char *l = buf;
+	char *h = &buf[len-1];
+	while (l < h) {
+		char temp = *h;
+		*h-- = *l;
+		*l++ = temp;
+	}
+	return len;
+}
+
+static int64_t iarg(size_t s, va_list arg)
+{
+	switch (s) {
+		case 1: return va_arg(arg, int); //int8_t
+		case 2: return va_arg(arg, int); //int16_t
+		case 4: return va_arg(arg, int32_t);
+		case 8: return va_arg(arg, int64_t);
+		default: return va_arg(arg, int);
+	}
+}
+
+static uint64_t uarg(size_t s, va_list arg)
+{
+	switch (s) {
+		case 1: return va_arg(arg, unsigned); //uint8_t
+		case 2: return va_arg(arg, unsigned); //uint16_t
+		case 4: return va_arg(arg, uint32_t);
+		case 8: return va_arg(arg, uint64_t);
+		default: return va_arg(arg, unsigned);
+	}
+}
+
 struct format_chunk _format_next(struct format_state *state, va_list arg)
 {
 	// Only call _format_next if you know there is more format string to read;
@@ -27,13 +68,15 @@ struct format_chunk _format_next(struct format_state *state, va_list arg)
 
 	// If the format string begins with literal text, scan up to the next
 	// format specifier, or the end of the string, and return that chunk.
-	struct format_chunk out = {state->fmt, 0};
-	while (*state->fmt != '\0' && *state->fmt != '%') {
-		state->fmt++;
-	}
-	out.size = state->fmt - out.addr;
-	if (out.size > 0) {
-		return out;
+	if (*state->fmt != '%') {
+		struct format_chunk out = {state->fmt, 0};
+		while (*state->fmt != '\0' && *state->fmt != '%') {
+			state->fmt++;
+		}
+		out.size = state->fmt - out.addr;
+		if (out.size > 0) {
+			return out;
+		}
 	}
 
 	// We did not find a literal text chunk, so we must have found a specifier.
@@ -110,23 +153,20 @@ struct format_chunk _format_next(struct format_state *state, va_list arg)
 		default: --fmt;
 	}
 
+	static const char *digits_lower = "0123456789abcdef";
+	static const char *digits_upper = "0123456789ABCDEF";
+
 	// Having parsed all of the parameters, if present, the remaining character
 	// is the format specifier.
 	char specifier = *fmt;
 	state->fmt = ++fmt;
+	char *dest = state->buffer;
 	switch (specifier) {
-		case '%': {
-			state->buffer[0] = '%';
-			out.addr = state->buffer;
-			out.size = 1;
-		} break;
 		case 'c': {
-			state->buffer[0] = va_arg(arg, int);
-			out.addr = state->buffer;
-			out.size = 1;
+			*dest++ = va_arg(arg, int);
 		} break;
 		case 's': {
-			out.addr = va_arg(arg, const char*);
+			struct format_chunk out = {va_arg(arg, const char*), 0};
 			if (out.addr) {
 				if (has_precision) {
 					const char *q = memchr(out.addr, '\0', precision);
@@ -135,9 +175,53 @@ struct format_chunk _format_next(struct format_state *state, va_list arg)
 					out.size = strlen(out.addr);
 				}
 			}
+			return out;
+		}
+		case 'i':
+		case 'd': {
+			int64_t num = iarg(length, arg);
+			if (num < 0) {
+				num = -num;
+				*dest++ = '-';
+			} else if (plus_for_positive) {
+				*dest++ = '+';
+			} else if (space_for_positive) {
+				*dest++ = ' ';
+			}
+			dest += utoa(dest, num, 10, digits_lower);
+		} break;
+		case 'x': {
+			if (alternate_form) {
+				*dest++ = '0';
+				*dest++ = 'x';
+			}
+			dest += utoa(dest, uarg(length, arg), 16, digits_lower);
+		} break;
+		case 'X': {
+			if (alternate_form) {
+				*dest++ = '0';
+				*dest++ = 'X';
+			}
+			dest += utoa(dest, uarg(length, arg), 16, digits_upper);
+		} break;
+		case 'o': {
+			if (alternate_form) {
+				*dest++ = '0';
+			}
+			dest += utoa(dest, uarg(length, arg), 8, digits_lower);
+		} break;
+		case '%':
+		default: {
+			*dest++ = specifier;
 		} break;
 	}
+	struct format_chunk out = {state->buffer, dest - state->buffer};
 	return out;
 }
 
+#ifdef TESTSUITE
+TESTSUITE(format) {
+
+}
+#endif
 
