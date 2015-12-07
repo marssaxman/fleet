8dcad16e13e87906c7f73f3d59d55e7b141e60a8
Mars Saxman <mars@redecho.org>
2015-12-06 19:10:14 -0800

    added tests for integer formatting specifiers

--- libc/src/stdio/format.c
+++ libc/src/stdio/format.c
@@ -220,11 +220,10 @@ struct format_chunk _format_next(struct format_state *state, va_list arg)
 }
 
 #ifdef TESTSUITE
-static const char *tdigs = "0123456789ABCDEF";
-char buf[FORMAT_BUFFER_SIZE];
 static char *testutoa(uint64_t n, int radix)
 {
-	buf[utoa(buf, n, radix, tdigs)] = '\0';
+	static char buf[FORMAT_BUFFER_SIZE];
+	buf[utoa(buf, n, radix, "0123456789ABCDEF")] = '\0';
 	return buf;
 }
 static char *enhex(uint64_t n)
@@ -239,27 +238,64 @@ static char *enoct(uint64_t n)
 {
 	return testutoa(n, 8);
 }
-TESTSUITE(utoa) {
-	CHECK_STR(enoct(0), "0", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enoct(1), "1", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(0), "0", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(1), "1", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(0), "0", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(1), "1", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(100), "100", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(127), "127", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(0x64), "64", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(0x7F), "7F", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(0x03E8), "3E8", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(32749), "32749", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(0xFFE1), "FFE1", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(0xFFFF), "FFFF", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(2305843009213693952LL), "2305843009213693952", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(0x7FFFFFFFFFFFFFFFULL), "7FFFFFFFFFFFFFFF", FORMAT_BUFFER_SIZE);
-	CHECK_STR(enhex(0xFFFFFFFFFFFFFFFFULL), "FFFFFFFFFFFFFFFF", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(9999999999999999999ULL), "9999999999999999999", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(9223372036854775807LL), "9223372036854775807", FORMAT_BUFFER_SIZE);
-	CHECK_STR(endec(18446744073709551615ULL), "18446744073709551615", FORMAT_BUFFER_SIZE);
+static char *enfmt(const char *fmt, ...)
+{
+	static struct format_state st;
+	_format_start(&st, fmt);
+	CHECK(!_format_done(&st));
+	va_list arg;
+	va_start(arg, fmt);
+	struct format_chunk chunk = _format_next(&st, arg);
+	va_end(arg);
+	static char buf[FORMAT_BUFFER_SIZE];
+	memcpy(buf, chunk.addr, chunk.size);
+	buf[chunk.size] = '\0';
+	return buf;
+}
+TESTSUITE(format) {
+	size_t size = FORMAT_BUFFER_SIZE;
+	CHECK_STR(enoct(0), "0", size);
+	CHECK_STR(enoct(1), "1", size);
+	CHECK_STR(endec(0), "0", size);
+	CHECK_STR(endec(1), "1", size);
+	CHECK_STR(enhex(0), "0", size);
+	CHECK_STR(enhex(1), "1", size);
+	CHECK_STR(endec(100), "100", size);
+	CHECK_STR(endec(127), "127", size);
+	CHECK_STR(enhex(0x64), "64", size);
+	CHECK_STR(enhex(0x7F), "7F", size);
+	CHECK_STR(enhex(0x03E8), "3E8", size);
+	CHECK_STR(endec(32749), "32749", size);
+	CHECK_STR(enhex(0xFFE1), "FFE1", size);
+	CHECK_STR(enhex(0xFFFF), "FFFF", size);
+	CHECK_STR(endec(2305843009213693952LL), "2305843009213693952", size);
+	CHECK_STR(enhex(0x7FFFFFFFFFFFFFFFULL), "7FFFFFFFFFFFFFFF", size);
+	CHECK_STR(enhex(0xFFFFFFFFFFFFFFFFULL), "FFFFFFFFFFFFFFFF", size);
+	CHECK_STR(endec(9999999999999999999ULL), "9999999999999999999", size);
+	CHECK_STR(endec(9223372036854775807LL), "9223372036854775807", size);
+	CHECK_STR(endec(18446744073709551615ULL), "18446744073709551615", size);
+	CHECK_STR(enoct(01777777777777777777777), "1777777777777777777777", size);
+	CHECK_STR(enfmt("%d", 1), "1", size);
+	CHECK_STR(enfmt("%i", 0), "0", size);
+	CHECK_STR(enfmt("%d", -1), "-1", size);
+	CHECK_STR(enfmt("%x", 42), "2a", size);
+	CHECK_STR(enfmt("%X", 42), "2A", size);
+	CHECK_STR(enfmt("%#X", 42), "0X2A", size);
+	CHECK_STR(enfmt("%#x", 42), "0x2a", size);
+	CHECK_STR(enfmt("%#o", 0), "00", size);
+	CHECK_STR(enfmt("%+i", 113976), "+113976", size);
+	CHECK_STR(enfmt("%0i", 113976), "113976", size);
+	CHECK_STR(enfmt("% i", 113976), " 113976", size);
+	CHECK_STR(enfmt("% i", -113976), "-113976", size);
+	CHECK_STR(enfmt("%s", "foo"), "foo", size);
+	CHECK_STR(enfmt("%c", 'Q'), "Q", size);
+	CHECK_STR(enfmt("%c", 0x41424344), "D", size);
+	CHECK_STR(enfmt("%hhX", 0xFEDCBA9876543210), "76543210", size);
+	CHECK_STR(enfmt("%hX", 0xFEDCBA9876543210), "76543210", size);
+	CHECK_STR(enfmt("%lX", 0xFEDCBA9876543210), "76543210", size);
+	CHECK_STR(enfmt("%llX", 0xFEDCBA9876543210), "FEDCBA9876543210", size);
+	CHECK_STR(enfmt("%%", 0), "%", size);
+	//CHECK_STR(enfmt("%Q", 0), "Q", size);
 }
 #endif
 
