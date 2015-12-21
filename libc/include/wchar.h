1d24b437c9e9e20afdf59db87a1bfd5b6c1c25e0
Mars Saxman <mars@redecho.org>
2015-12-21 11:27:21 -0800

    added two missing wchar string functions

--- libc/include/wchar.h
+++ libc/include/wchar.h
@@ -15,12 +15,45 @@
 #include <_weof.h>
 #include <_wchar_min_max.h>
 
-size_t wcslen(const wchar_t *s);
-wchar_t *wcschr(const wchar_t *s, wchar_t c);
+// fgetwc
+// fgetws
+// fputwc
+// fputws
+// fwide
+// fwprintf
+// fwscanf
+// getwc
+// getwchar
+// putwc
+// putwchar
+// swprintf
+// swscanf
+// ungetwc
+// vfwprintf
+// vwprintf
+// wprintf
+// wscanf
+
+// wcstod
+// wcstol
+// wcstoul
+
+// btowc
+// mbrlen
+// mbrtowc
+// mbsinit
+// mbsrtowcs
+// wcrtomb
+// wctob
+// wcsrtombs
+
 wchar_t *wcscat(wchar_t *restrict dest, const wchar_t *restrict src);
+wchar_t *wcschr(const wchar_t *s, wchar_t c);
 int wcscmp(const wchar_t *l, const wchar_t *r);
+int wcscoll(const wchar_t *l, const wchar_t *r);
 wchar_t *wcscpy(wchar_t *restrict d, const wchar_t *restrict s);
 size_t wcscspn(const wchar_t *s, const wchar_t *c);
+size_t wcslen(const wchar_t *s);
 wchar_t *wcsncat(wchar_t *restrict d, const wchar_t *restrict s, size_t n);
 int wcsncmp(const wchar_t *l, const wchar_t *r, size_t n);
 wchar_t *wcsncpy(wchar_t *restrict d, const wchar_t *restrict s, size_t n);
@@ -29,55 +62,14 @@ wchar_t *wcsrchr(const wchar_t *s, wchar_t c);
 size_t wcsspn(const wchar_t *s, const wchar_t *c);
 wchar_t *wcsstr(const wchar_t *restrict h, const wchar_t *restrict n);
 wchar_t *wcstok(wchar_t *restrict s, const wchar_t *restrict sep, wchar_t **restrict p);
-wchar_t *wmemset(wchar_t *d, wchar_t c, size_t n);
-int wmemcmp(const wchar_t *l, const wchar_t *r, size_t n);
+size_t wcsxfrm(wchar_t *restrict dest, const wchar_t *restrict src, size_t n);
 wchar_t *wmemchr(const wchar_t *s, wchar_t c, size_t n);
+int wmemcmp(const wchar_t *l, const wchar_t *r, size_t n);
 wchar_t *wmemcpy(wchar_t *restrict d, const wchar_t *restrict s, size_t n);
 wchar_t *wmemmove(wchar_t *d, const wchar_t *s, size_t n);
+wchar_t *wmemset(wchar_t *d, wchar_t c, size_t n);
 
-#if 0 // still to implement
-btowc
-fgetwc
-fgetws
-fputwc
-fputws
-fwide
-fwprintf
-fwscanf
-getwc
-getwchar
-mbrlen
-mbrtowc
-mbsinit
-mbsrtowcs
-mbstate_t
-putwc
-putwchar
-swprintf
-swscanf
-ungetwc
-vfwprintf
-vfwscanf
-vswprintf
-vswscanf
-vwprintf
-vwscanf
-wcrtomb
-wcscoll
-wcsftime
-wcsrtombs
-wcstod
-wcstof
-wcstol
-wcstold
-wcstoll
-wcstoul
-wcstoull
-wcsxfrm
-wctob
-wprintf
-wscanf
-#endif // not yet implemented
+// wcsftime
 
 #endif // _WCHAR_H
 
---
+++ libc/src/wchar/wcscoll.c
@@ -0,0 +1,7 @@
+#include <wchar.h>
+
+int wcscoll(const wchar_t *l, const wchar_t *r)
+{
+	return wcscmp(l, r);
+}
+
