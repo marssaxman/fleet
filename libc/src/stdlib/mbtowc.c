6790427e97482c01a82640511f36ec784a7d90ff
Mars Saxman <mars@redecho.org>
2015-12-21 10:04:25 -0800

    added tests for UTF-8 encoder and decoder

--- libc/src/stdlib/mbtowc.c
+++ libc/src/stdlib/mbtowc.c
@@ -27,7 +27,7 @@ int mbtowc(wchar_t *dest, const char *src, size_t max)
 	// 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx: 21 bits
 	// UTF-8 can encode 26 and 32-bit sequences, but those are outside the
 	// Unicode range, so we don't accept them.
-	switch (c >> 4) {
+	switch ((c >> 4) & 0x0F) {
 		case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
 			// 1-byte ascii
 			if (max < 1) goto fail;
@@ -63,4 +63,70 @@ fail:
 	errno = EILSEQ;
 	return -1;
 }
+
+#ifdef TESTSUITE
+#include <string.h>
+TESTSUITE(mbtowc_match) {
+	struct check {
+		wchar_t expected;
+		const char *src;
+	} examples[] = {
+		{0, ""},
+		{L'a', "a"},
+		{0xD7FF, "\xED\x9F\xBF"},
+		{0xE000, "\xEE\x80\x80"},
+		{0xFFFD, "\xEF\xBF\xBD"},
+		{0x10FFFF, "\xF4\x8F\xBF\xBF"},
+		{0xFFFE, "\xEF\xBF\xBE"},
+		{0xFFFF, "\xEF\xBF\xBF"},
+		{0x7F, "\x7F"},
+		{0x80, "\xC2\x80"},
+		{0xFF, "\xC3\xBF"},
+		{0x0100, "\xC4\x80"},
+		{0x7FF, "\xDF\xBF"},
+		{0x0800, "\xE0\xA0\x80"},
+		{0x0FFF, "\xE0\xBF\xBF"},
+		{0x1000, "\xE1\x80\x80"},
+		{0x10000, "\xF0\x90\x80\x80"},
+		{0x11080, "\xF0\x91\x82\x80"},
+		{0xE0000, "\xF3\xA0\x80\x80"},
+		{0x10FFFF, "\xF4\x8F\xBF\xBF"},
+		{0, 0}
+	};
+	for (struct check *i = examples; i->src; ++i) {
+		wchar_t dest = 0;
+		size_t len = strlen(i->src);
+		CHECK(len == mbtowc(&dest, i->src, len));
+		CHECK(dest == i->expected);
+	}
+}
+TESTSUITE(mbtowc_fail) {
+	struct check {
+		const char *src;
+	} examples[] = {
+		"\x80",
+		"\xF3\x80",
+		"\xF3\x80\x80",
+		"\xBF",
+		"\xE0 ",
+		"\xE1 ",
+		"\xE2 ",
+		"\xEF ",
+		"\xF0 ",
+		"\xF1 ",
+		"\xFA ",
+		"\xFB ",
+		"\xFC\x80\x80\x80\x80\x80",
+		"\xFE",
+		"\xFF",
+		"\xFE\xFE\xFF\xFF",
+		0
+	};
+	for (struct check *i = examples; i->src; ++i) {
+		wchar_t dest = 0;
+		size_t len = strlen(i->src);
+		CHECK(-1 == mbtowc(&dest, i->src, len));
+	}
+}
+#endif
 
--- libc/src/stdlib/wctomb.c
+++ libc/src/stdlib/wctomb.c
@@ -25,7 +25,7 @@ int wctomb(char *dest, wchar_t wc)
 		dest[1] = ((wc >> 6) & 0x3F) | 0x80;
 		dest[2] = (wc & 0x3F) | 0x80;
 		return 3;
-	} else if (wc < 0x200000) {
+	} else if (wc < 0x110000) {
 		// 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx: 21 bits
 		dest[0] = ((wc >> 18) & 0x07) | 0xF0;
 		dest[1] = ((wc >> 12) & 0x3F) | 0x80;
@@ -36,4 +36,56 @@ int wctomb(char *dest, wchar_t wc)
 		return -1;
 	}
 }
+
+#ifdef TESTSUITE
+#include <string.h>
+#include <limits.h>
+TESTSUITE(wctomb_match) {
+	struct check {
+		const char *expected;
+		wchar_t src;
+	} examples[] = {
+		{"", 0},
+		{"a", L'a'},
+		{"\xED\x9F\xBF", 0xD7FF},
+		{"\xEE\x80\x80", 0xE000},
+		{"\xEF\xBF\xBD", 0xFFFD},
+		{"\xF4\x8F\xBF\xBF", 0x10FFFF},
+		{"\xEF\xBF\xBE", 0xFFFE},
+		{"\xEF\xBF\xBF", 0xFFFF},
+		{"\x7F", 0x7F},
+		{"\xC2\x80", 0x80},
+		{"\xC3\xBF", 0xFF},
+		{"\xC4\x80", 0x0100},
+		{"\xDF\xBF", 0x7FF},
+		{"\xE0\xA0\x80", 0x0800},
+		{"\xE0\xBF\xBF", 0x0FFF},
+		{"\xE1\x80\x80", 0x1000},
+		{"\xF0\x90\x80\x80", 0x10000},
+		{"\xF0\x91\x82\x80", 0x11080},
+		{"\xF3\xA0\x80\x80", 0xE0000},
+		{"\xF4\x8F\xBF\xBF", 0x10FFFF},
+		{0, 0}
+	};
+	for (struct check *i = examples; i->src; ++i) {
+		char dest[MB_LEN_MAX];
+		size_t len = strlen(i->expected);
+		CHECK(len == wctomb(dest, i->src));
+		CHECK_STR(dest, i->expected, MB_LEN_MAX);
+	}
+}
+TESTSUITE(wctomb_fail) {
+	wchar_t examples[] = {
+		0x001FFFFF,
+		0x03FFFFFF,
+		0x7FFFFFFF,
+		0x00110000,
+		0
+	};
+	for (wchar_t *i = examples; *i; ++i) {
+		char dest[MB_LEN_MAX];
+		CHECK((size_t)-1 == wctomb(dest, *i));
+	}
+}
+#endif
 
