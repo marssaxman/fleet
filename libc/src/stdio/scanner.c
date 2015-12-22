4e4103ec670238ae17f19e0369b6f4979f57179e
Mars Saxman <mars@redecho.org>
2015-12-22 09:03:40 -0800

    more work on scanf state machine

--- libc/src/stdio/scanner.c
+++ libc/src/stdio/scanner.c
@@ -15,8 +15,11 @@ enum mode {
 	MODE_SPACE,
 	MODE_INTEGER,
 	MODE_DEC,
-	MODE_OCT,
-	MODE_HEX,
+	MODE_OCT_OR_HEX,
+	MODE_OCT_BEGIN,
+	MODE_OCT_BODY,
+	MODE_HEX_BEGIN,
+	MODE_HEX_BODY,
 	MODE_FLOAT,
 	MODE_FIX,
 	MODE_EXP,
@@ -28,7 +31,6 @@ enum mode {
 	MODE_NEGSET,
 	MODE_NUM,
 	MODE_LITERAL,
-	MODE_NEXT,
 	MODE_DONE,
 	NUM_MODES
 };
@@ -99,6 +101,22 @@ static void parse_length(struct scanner_state *state)
 
 static void parse_scanset(struct scanner_state *state)
 {
+	if ('^' == *++state->format) {
+		state->mode = MODE_NEGSET;
+		++state->format;
+	} else {
+		state->mode = MODE_SCANSET;
+	}
+	state->specifier = state->format;
+	state->spec_size = 0;
+	while (1) {
+		switch (*state->format) {
+			case ']': ++state->format;
+			case '\0': return;
+			default: ++state->spec_size;
+		}
+		++state->format;
+	}
 }
 
 static void parse_specifier(struct scanner_state *state)
@@ -106,12 +124,12 @@ static void parse_specifier(struct scanner_state *state)
 	state->specifier = state->format;
 	state->spec_size = 1;
 	switch (*state->format) {
-		case '\0': state->mode = MODE_DONE; return;
+		case '\0': return;
 		case 'i':
 		case 'u': state->mode = MODE_INTEGER; break;
 		case 'd': state->mode = MODE_DEC; break;
-		case 'o': state->mode = MODE_OCT; break;
-		case 'x': state->mode = MODE_HEX; break;
+		case 'o': state->mode = MODE_OCT_BEGIN; break;
+		case 'x': state->mode = MODE_HEX_BEGIN; break;
 		case 'f': state->mode = MODE_FIX; break;
 		case 'e': state->mode = MODE_EXP; break;
 		case 'g': state->mode = MODE_FLOAT; break;
@@ -134,11 +152,11 @@ static void mode_parse(struct scanner_state *state)
 	state->spec_size = 0;
 	state->dest = 0;
 	state->specifier = state->format;
-	char ch = *state->format++;
+	char ch = *state->format;
+	if (0 == ch) return;
+	++state->format;
 	if (isspace(ch)) {
 		state->mode = MODE_SPACE;
-	} else if ('\0' == ch) {
-		state->mode = MODE_DONE;
 	} else if ('%' == ch) {
 		// %[*][width][length]specifier
 		parse_dest(state);
@@ -152,21 +170,53 @@ static void mode_parse(struct scanner_state *state)
 
 static void mode_space(struct scanner_state *state)
 {
+	if (isspace(state->current)) {
+		state->mode = MODE_SPACE;
+		state->current = 0;
+	} else {
+		state->mode = MODE_PARSE;
+	}
 }
 
 static void mode_integer(struct scanner_state *state)
 {
+	// Potentially any kind of integer: we figure out which kind by looking
+	// at the first digit. If it's a leading zero, it must be hex or octal.
+	if ('0' == state->current) {
+		state->mode = MODE_OCT_OR_HEX;
+		state->current = 0;
+	} else if (isdigit(state->current)) {
+		state->mode = MODE_DEC;
+	}
 }
 
 static void mode_dec(struct scanner_state *state)
 {
 }
 
-static void mode_oct(struct scanner_state *state)
+static void mode_oct_or_hex(struct scanner_state *state)
+{
+	if ('x' == state->current || 'X' == state->current) {
+		state->mode = MODE_HEX_BODY;
+		++state->format;
+	} else {
+		state->mode = MODE_OCT_BODY;
+	}
+}
+
+static void mode_oct_begin(struct scanner_state *state)
 {
 }
 
-static void mode_hex(struct scanner_state *state)
+static void mode_oct_body(struct scanner_state *state)
+{
+}
+
+static void mode_hex_begin(struct scanner_state *state)
+{
+}
+
+static void mode_hex_body(struct scanner_state *state)
 {
 }
 
@@ -214,11 +264,6 @@ static void mode_literal(struct scanner_state *state)
 {
 }
 
-static void mode_next(struct scanner_state *state)
-{
-	state->current = 0;
-}
-
 static void mode_done(struct scanner_state *state)
 {
 	state->current = 0;
@@ -232,8 +277,11 @@ bool _scanner_next(struct scanner_state *state, char ch)
 		mode_parse,
 		mode_integer,
 		mode_dec,
-		mode_oct,
-		mode_hex,
+		mode_oct_or_hex,
+		mode_oct_begin,
+		mode_oct_body,
+		mode_hex_begin,
+		mode_hex_body,
 		mode_float,
 		mode_fix,
 		mode_exp,
@@ -250,7 +298,7 @@ bool _scanner_next(struct scanner_state *state, char ch)
 	state->current = ch;
 	while (state->current) {
 		proc p = funcs[state->mode];
-		state->mode = MODE_NEXT;
+		state->mode = MODE_DONE;
 		p(state);
 	}
 	return state->mode != MODE_DONE;
