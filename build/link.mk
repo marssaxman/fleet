c62f75012d3bc5e6a0e0d927db1d854d183e1151
Mars Saxman <mars@redecho.org>
2016-04-16 22:59:25 -0700

    update build flags for better C/C++ universality

--- build/link.mk
+++ build/link.mk
@@ -1,9 +1,9 @@
 # Include this from your Makefile to build your executable as a bootable kernel
 # image for i386 using libfleet's C runtime and standard library.
-FLEETDIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST)))/..)
+FLEETDIR:=$(realpath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 include $(FLEETDIR)/build/target.mk
-CFLAGS += -isystem $(FLEETDIR)/libc/include
-LDLIBS += -lc -lkernel
-LDFLAGS += -L$(FLEETDIR)/libc -L$(FLEETDIR)/kernel
-LDFLAGS += -T $(FLEETDIR)/build/linker.ld
+CPPFLAGS+=-isystem $(FLEETDIR)/libc/include -isystem $(FLEETDIR)/kernel/include
+LDLIBS+=-lc -lkernel
+LDFLAGS+=-L$(FLEETDIR)/libc -L$(FLEETDIR)/kernel
+LDFLAGS+=-T $(FLEETDIR)/build/linker.ld
 
