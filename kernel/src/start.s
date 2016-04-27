80a7d84e6289a2cd919f9f1b2e31c6422b8f311b
Mars Saxman <mars@redecho.org>
2016-04-27 13:30:46 -0700

    didn't like that formatting experiment after all

--- kernel/src/start.s
+++ kernel/src/start.s
@@ -4,29 +4,27 @@
 # this paragraph and the above copyright notice. THIS SOFTWARE IS PROVIDED "AS
 # IS" WITH NO EXPRESS OR IMPLIED WARRANTY.
 
-.global _start, _kernel
-
 .section .text
-_start:
-# Make sure EAX contains the multiboot magic code, because we depend on the
-# multiboot information record which should be in EBX.
+_start: .global _start
+	# Make sure EAX contains the multiboot magic code, because we depend on the
+	# multiboot information record which should be in EBX.
 	cmpl $0x2BADB002, %eax
 	jne .Lhang
 
-# Make sure the multiboot info has lower/upper memory information.
+	# Make sure the multiboot info has lower/upper memory information.
 	testb $1, (%ebx)
 	jz .Lhang
 
-# This is a single-stack kernel, so we'll place the base of the stack at the
-# end of upper memory. Upper memory starts at 0x00100000, and the multiboot
-# info record contains the number of 1K blocks it contains.
+	# This is a single-stack kernel, so we'll place the base of the stack at the
+	# end of upper memory. Upper memory starts at 0x00100000, and the multiboot
+	# info record contains the number of 1K blocks it contains.
 	movl $0x00100000, %esp
 	movl 8(%ebx), %eax
 	rol $10, %eax
 	addl %eax, %esp
 
-# The bootloader should have given us a flat 32-bit address space, but we
-# aren't supposed to assume that, so we'll install our own GDT anyway.
+	# The bootloader should have given us a flat 32-bit address space, but we
+	# aren't supposed to assume that, so we'll install our own GDT anyway.
 	lgdtl (gdtr)
 	mov $0x10, %ax
 	mov %ax, %ds
@@ -36,11 +34,11 @@ _start:
 	mov %ax, %ss
 	ljmp $0x8, $0f; 0:
 
-# Call the kernel entrypoint, passing in the multiboot info.
+	# Call the kernel entrypoint, passing in the multiboot info.
 	pushl %ebx
 	call _kernel
 
-# The program is done. Halt, and halt in a loop in case an NMI wakes us up.
+	# The program is done. Halt, and halt in a loop in case an NMI wakes us up.
 .Lhang:
 	cli
 	hlt
@@ -61,7 +59,8 @@ gdt:
 	.byte 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x92, 0xCF, 0x00 # system data
 	.byte 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFA, 0xCF, 0x00 # user code
 	.byte 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xF2, 0xCF, 0x00 # user data
+.equ GDT_SIZE, .-gdt; .local GDT_SIZE
 gdtr:
-	.hword 39	# limit: size of table in bytes, minus one
+	.hword GDT_SIZE-1
 	.long gdt	# base: address of table
 
